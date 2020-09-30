f92187dcd0c2fd7f6c686053d72ca5bd39fdd63a
Harpreet Ghotra <harpreetghotra40@icloud.com>
2020-09-30 11:47:33 -0400

    Updates

---
+++ precache-manifest.d7e850f6dc9c71122611bf75f07425de.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8634c2c8c62748ccd5006fbf69fa997",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5508810c449571a82651",
+    "url": "/portfolio/static/css/main.57ba7e62.chunk.css"
+  },
+  {
+    "revision": "2335c57a74297b0b08e5",
+    "url": "/portfolio/static/js/2.79e9ed1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.79e9ed1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5508810c449571a82651",
+    "url": "/portfolio/static/js/main.c4f574c3.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "fa38ab30a9b8abb4a1ff8c0b7fb86cef",
+    "url": "/portfolio/static/media/gatsby.fa38ab30.png"
+  },
+  {
+    "revision": "8e82ae28b5907456be28d654232a3e03",
+    "url": "/portfolio/static/media/mailgun.8e82ae28.png"
+  },
+  {
+    "revision": "54248edab31571cfa6090839b5b8cc88",
+    "url": "/portfolio/static/media/praluent.54248eda.png"
+  },
+  {
+    "revision": "a10d50336db650a46fe20c82e144992f",
+    "url": "/portfolio/static/media/praluenthcp.a10d5033.png"
+  },
+  {
+    "revision": "5d243e7c1fc81c578b8362769fbf7e91",
+    "url": "/portfolio/static/media/reactjs.5d243e7c.png"
+  },
+  {
+    "revision": "b535a1d1e2d9eb17ec5464c119cf0ebe",
+    "url": "/portfolio/static/media/sagar_lg.b535a1d1.png"
+  },
+  {
+    "revision": "5ae40b03dcf0e76f8e91c3129d9b76cd",
+    "url": "/portfolio/static/media/sass.5ae40b03.png"
+  },
+  {
+    "revision": "46d7256e8e3fa41d1144d82c4c422d96",
+    "url": "/portfolio/static/media/sciencefoundry.46d7256e.png"
+  },
+  {
+    "revision": "8a764b8071a2f9abe85dbbd80d9148b8",
+    "url": "/portfolio/static/media/vascepa.8a764b80.png"
+  }
+]);
